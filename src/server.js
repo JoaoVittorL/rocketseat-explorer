75e2cbeee9c877db17b54fdd07f2c769f4708d5b
João Vittor  Lopes <108017312+JoaoVittorL@users.noreply.github.com>
2023-10-04 23:54:29 -0300

    create table users in sqlite

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require("express-async-errors");
-const database = require("./database/sqlite");
+const migrationRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 
 const express = require("express");
 const routes = require("./routes");
+
+migrationRun();
 
 const app = express();
 
@@ -11,7 +13,6 @@ const port = 3000;
 app.use(express.json());
 
 app.use(routes);
-database();
 
 app.use((error, req, res, next) => {
   if (error instanceof AppError) {
